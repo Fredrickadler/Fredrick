85b5f218fdd49bd5a6127eea20ac1c42f86f7757
Fredrickadler <13fredrick2001@gmail.com>
2025-09-05 09:57:19 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,20 @@
-.
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Vazir', 'IranSans', 'Shabnam', 'sans-serif'],
+      },
+      colors: {
+        primary: '#4F46E5',
+        secondary: '#F59E0B',
+      },
+    },
+  },
+  plugins: [],
+}
